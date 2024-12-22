ed4de92e46f6687bd1725421e65a8f32a0a30bab
AsfaaKhan <khanzadiasfi0314@gmail.com>
2024-12-22 18:09:16 +0500

    Data Fatch Side

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,8 +10,8 @@ export default function Header() {
                 <div className="mx-auto text-bold text-xl justify-center flex items-center text-center ">
                     <ul className="flex gap-10 cursor-pointer  ">
                         <li className="hover:text-blue-500   hover:border-[1px] rounded-full p-2  "><Link href={"/"}>Home</Link></li>
-                        <li className="hover:text-blue-500   hover:border-[1px] rounded-full p-2  "><Link href={"/clientSide"}>Client Side Fetch</Link></li>
-                        <li className="hover:text-blue-500   hover:border-[1px] rounded-full p-2 "><Link href={"serverSide"}>Server Side Fetch</Link></li>
+                        <li className="hover:text-blue-500   hover:border-[1px] rounded-full p-2  "><Link href={"/clientSide"}>Client Side </Link></li>
+                        <li className="hover:text-blue-500   hover:border-[1px] rounded-full p-2 "><Link href={"serverSide"}>Server Side </Link></li>
                     </ul>
                 </div>
             </nav>
